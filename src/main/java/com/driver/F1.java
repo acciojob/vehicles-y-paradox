fd2c3ca1c40b55d8ebcc4dd86b2985e3d31e1ebd
y-paradox <yadavakshat586@gmail.com>
2023-02-23 20:31:34 +0530

    Vehicle Solution 1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,11 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 4, 5, isManual, "luxury", 6);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,8 +19,24 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
+        setCurrentSpeed(newSpeed);
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
+        } else if (newSpeed <= 50) {
+            changeGear(1);
+        } else if (newSpeed <= 100) {
+            changeGear(2);
+        } else if (newSpeed <= 150) {
+            changeGear(3);
+        } else if (newSpeed <= 200) {
+            changeGear(4);
+        } else if (newSpeed <= 250) {
+            changeGear(5);
+        }
+        else{
+            changeGear(6) ;
         }
         //for all other cases, change the gear accordingly
 
